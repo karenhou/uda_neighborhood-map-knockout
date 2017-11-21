4f00ab215f89ea28ce7fbae0a77ecfc45a3d6578
Karen Hou <ih2g10@gmail.com>
2017-11-21 14:48:06 +0800

    Move model data to data.js

---
+++ js/data.js
@@ -0,0 +1,34 @@
+// models
+var initLocations = [{
+        title: 'messe frankfurt',
+        latLng: { lat: 50.1102447, lng: 8.6483381 }
+    },
+    {
+        title: 'Städel Museum',
+        latLng: { lat: 50.1030925, lng: 8.6740583 }
+    },
+    {
+        title: 'Römerberg',
+        latLng: { lat: 50.1107073, lng: 8.6819623 }
+    },
+    {
+        title: 'Kleinmarkthalle',
+        latLng: { lat: 50.1128541, lng: 8.6836329 }
+    },
+    {
+        title: 'Kaiserdom St. Bartholomäus',
+        latLng: { lat: 50.1106631, lng: 8.6854204 }
+    },
+    {
+        title: 'Gerechtigkeitsbrunnen',
+        latLng: { lat: 50.1104364, lng: 8.682155 }
+    },
+    {
+        title: 'The Hauptwache',
+        latLng: { lat: 50.1134865, lng: 8.6787432 }
+    },
+    {
+        title: 'MyZeil',
+        latLng: { lat: 50.1143519, lng: 8.6814498 }
+    }
+];
